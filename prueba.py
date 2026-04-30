64b850707e87b487ea6c408044049d5345661557
jherson2024 <159274759+jherson2024@users.noreply.github.com>
2026-04-30 16:39:16 -0500

    Delete prueba.py

--- prueba.py
+++
@@ -1,7 +0,0 @@
-Característica: Calculadora de reembolsos
-
-  Escenario: Cliente VIP cancela con 2 horas de anticipación
-    Dado que el monto de la reserva es 200
-    Y el cliente es VIP
-    Cuando cancela con 2 horas de anticipación
-    Entonces recibe un reembolso de 100
